4e11425f794c1ad7db3c875259edc67facf31d44
David Robillard <d@drobilla.net>
2011-10-10 01:41:53 +0000

    Switch to ISC license.

--- core.lv2/lv2.h
+++ core.lv2/lv2.h
@@ -1,31 +1,27 @@
 /*
   LV2 - An audio plugin interface specification.
-  Copyright (C) 2006-2011 Steve Harris, David Robillard.
+  Copyright 2006-2011 Steve Harris, David Robillard.
 
-  Based on LADSPA:
-  Copyright (C) 2000-2002 Richard W.E. Furse, Paul Barton-Davis,
-                          Stefan Westerfeld.
+  Based on LADSPA, Copyright 2000-2002 Richard W.E. Furse,
+  Paul Barton-Davis, Stefan Westerfeld.
  
-  This header is free software; you can redistribute it and/or modify it
-  under the terms of the GNU Lesser General Public License as published
-  by the Free Software Foundation; either version 2.1 of the License,
-  or (at your option) any later version.
- 
-  This header is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-  Lesser General Public License for more details.
- 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
-  USA.
+  Permission to use, copy, modify, and/or distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+
+  THIS SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
 
 /**
   @file lv2.h
   API for the LV2 specification <http://lv2plug.in/ns/lv2core>.
-  Revision: 3.2
+  Revision: 4.1
 */
 
 #ifndef LV2_H_INCLUDED
