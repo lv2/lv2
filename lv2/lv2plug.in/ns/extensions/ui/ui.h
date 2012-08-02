5defc8ead1e8d996005b94741ecaf0fcec4e1482
David Robillard <d@drobilla.net>
2012-08-02 00:47:11 +0000

    Add C defines for ui:WindowsUI and ui:CocoaUI.

--- lv2/lv2plug.in/ns/extensions/ui/ui.h
+++ lv2/lv2plug.in/ns/extensions/ui/ui.h
@@ -31,11 +31,13 @@
 #define LV2_UI_URI    "http://lv2plug.in/ns/extensions/ui"
 #define LV2_UI_PREFIX LV2_UI_URI "#"
 
+#define LV2_UI__CocoaUI          LV2_UI_PREFIX "CocoaUI"
 #define LV2_UI__Gtk3UI           LV2_UI_PREFIX "Gtk3UI"
 #define LV2_UI__GtkUI            LV2_UI_PREFIX "GtkUI"
 #define LV2_UI__PortNotification LV2_UI_PREFIX "PortNotification"
 #define LV2_UI__Qt4UI            LV2_UI_PREFIX "Qt4UI"
 #define LV2_UI__UI               LV2_UI_PREFIX "UI"
+#define LV2_UI__WindowsUI        LV2_UI_PREFIX "WindowsUI"
 #define LV2_UI__X11UI            LV2_UI_PREFIX "X11UI"
 #define LV2_UI__binary           LV2_UI_PREFIX "binary"
 #define LV2_UI__fixedSize        LV2_UI_PREFIX "fixedSize"
