54bad1e397679efca7cfaa3a174d618f74e72637
David Robillard <d@drobilla.net>
2015-10-16 23:34:40 -0400

    Remove unused URIs from eg-fifths
    Patch from Geoff Hill

--- plugins/eg-fifths.lv2/uris.h
+++ plugins/eg-fifths.lv2/uris.h
@@ -1,6 +1,6 @@
 /*
   LV2 Fifths Example Plugin
-  Copyright 2014 David Robillard <d@drobilla.net>
+  Copyright 2014-2015 David Robillard <d@drobilla.net>
 
   Permission to use, copy, modify, and/or distribute this software for any
   purpose with or without fee is hereby granted, provided that the above
@@ -22,10 +22,7 @@
 #include "lv2/lv2plug.in/ns/ext/midi/midi.h"
 #include "lv2/lv2plug.in/ns/ext/state/state.h"
 
-#define EG_FIFTHS_URI          "http://lv2plug.in/plugins/eg-fifths"
-#define EG_FIFTHS__sample      EG_FIFTHS_URI "#sample"
-#define EG_FIFTHS__applySample EG_FIFTHS_URI "#applySample"
-#define EG_FIFTHS__freeSample  EG_FIFTHS_URI "#freeSample"
+#define EG_FIFTHS_URI "http://lv2plug.in/plugins/eg-fifths"
 
 typedef struct {
 	LV2_URID atom_Blank;
@@ -34,9 +31,6 @@ typedef struct {
 	LV2_URID atom_Sequence;
 	LV2_URID atom_URID;
 	LV2_URID atom_eventTransfer;
-	LV2_URID eg_applySample;
-	LV2_URID eg_sample;
-	LV2_URID eg_freeSample;
 	LV2_URID midi_Event;
 	LV2_URID patch_Set;
 	LV2_URID patch_property;
@@ -52,9 +46,6 @@ map_fifths_uris(LV2_URID_Map* map, FifthsURIs* uris)
 	uris->atom_Sequence      = map->map(map->handle, LV2_ATOM__Sequence);
 	uris->atom_URID          = map->map(map->handle, LV2_ATOM__URID);
 	uris->atom_eventTransfer = map->map(map->handle, LV2_ATOM__eventTransfer);
-	uris->eg_applySample     = map->map(map->handle, EG_FIFTHS__applySample);
-	uris->eg_freeSample      = map->map(map->handle, EG_FIFTHS__freeSample);
-	uris->eg_sample          = map->map(map->handle, EG_FIFTHS__sample);
 	uris->midi_Event         = map->map(map->handle, LV2_MIDI__MidiEvent);
 	uris->patch_Set          = map->map(map->handle, LV2_PATCH__Set);
 	uris->patch_property     = map->map(map->handle, LV2_PATCH__property);
