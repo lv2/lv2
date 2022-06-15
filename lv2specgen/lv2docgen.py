bae9223780400754040e2d25035a86e5efef5b3e
David Robillard <d@drobilla.net>
2022-06-15 13:02:14 -0400

    Use "python3" in shebangs
    Python 2 hit EOL long ago, and some systems don't have a "python" at all.

--- lv2specgen/lv2docgen.py
+++ lv2specgen/lv2docgen.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # lv2docgen, a documentation generator for LV2 plugins
--- plugins/literasc.py
+++ plugins/literasc.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # Literasc, a simple literate programming tool for C, C++, and Turtle.
