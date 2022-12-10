86e5e821531ff563b7e4642aa792d9e0f3067557
David Robillard <d@drobilla.net>
2022-12-10 14:51:34 -0500

    Test headers for warnings more strictly

---
+++ test/headers/test_headers.c
@@ -0,0 +1,42 @@
+// Copyright 2022 David Robillard <d@drobilla.net>
+// SPDX-License-Identifier: ISC
+
+#include "lv2/atom/atom.h"                       // IWYU pragma: keep
+#include "lv2/atom/forge.h"                      // IWYU pragma: keep
+#include "lv2/atom/util.h"                       // IWYU pragma: keep
+#include "lv2/buf-size/buf-size.h"               // IWYU pragma: keep
+#include "lv2/core/attributes.h"                 // IWYU pragma: keep
+#include "lv2/core/lv2.h"                        // IWYU pragma: keep
+#include "lv2/core/lv2_util.h"                   // IWYU pragma: keep
+#include "lv2/data-access/data-access.h"         // IWYU pragma: keep
+#include "lv2/dynmanifest/dynmanifest.h"         // IWYU pragma: keep
+#include "lv2/event/event-helpers.h"             // IWYU pragma: keep
+#include "lv2/event/event.h"                     // IWYU pragma: keep
+#include "lv2/instance-access/instance-access.h" // IWYU pragma: keep
+#include "lv2/log/log.h"                         // IWYU pragma: keep
+#include "lv2/log/logger.h"                      // IWYU pragma: keep
+#include "lv2/midi/midi.h"                       // IWYU pragma: keep
+#include "lv2/morph/morph.h"                     // IWYU pragma: keep
+#include "lv2/options/options.h"                 // IWYU pragma: keep
+#include "lv2/parameters/parameters.h"           // IWYU pragma: keep
+#include "lv2/patch/patch.h"                     // IWYU pragma: keep
+#include "lv2/port-groups/port-groups.h"         // IWYU pragma: keep
+#include "lv2/port-props/port-props.h"           // IWYU pragma: keep
+#include "lv2/presets/presets.h"                 // IWYU pragma: keep
+#include "lv2/resize-port/resize-port.h"         // IWYU pragma: keep
+#include "lv2/state/state.h"                     // IWYU pragma: keep
+#include "lv2/time/time.h"                       // IWYU pragma: keep
+#include "lv2/ui/ui.h"                           // IWYU pragma: keep
+#include "lv2/units/units.h"                     // IWYU pragma: keep
+#include "lv2/uri-map/uri-map.h"                 // IWYU pragma: keep
+#include "lv2/urid/urid.h"                       // IWYU pragma: keep
+#include "lv2/worker/worker.h"                   // IWYU pragma: keep
+
+#ifdef __GNUC__
+__attribute__((const))
+#endif
+int
+main(void)
+{
+  return 0;
+}
