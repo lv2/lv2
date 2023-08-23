5bb15b2b62fe1fe25028b29f1243757fdb8c7dbf
David Robillard <d@drobilla.net>
2023-08-23 11:14:02 -0400

    Suppress clang-tidy warning

--- include/lv2/atom/forge.h
+++ include/lv2/atom/forge.h
@@ -147,6 +147,7 @@ lv2_atom_forge_init(LV2_Atom_Forge* forge, LV2_URID_Map* map)
 static inline LV2_Atom*
 lv2_atom_forge_deref(LV2_Atom_Forge* forge, LV2_Atom_Forge_Ref ref)
 {
+  // NOLINTNEXTLINE(performance-no-int-to-ptr)
   return forge->buf ? (LV2_Atom*)ref : forge->deref(forge->handle, ref);
 }
 
